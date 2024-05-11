f887da6e5fc46782dd327bc3196a75ceb139d3e5
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-11 17:14:40 +0300

    BREAKING CHANGE: moving to nextjs instead of react vite

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useTelegram } from "@/providers/telegram-provider";
+
+export default function Home() {
+    const { user, webApp } = useTelegram();
+    console.log(user, webApp);
+    return (
+        <main>
+            <div>
+                <pre className="text-black">{JSON.stringify([1, 2, 3, 4], null, 2)}</pre>
+                <pre className="text-black">{JSON.stringify([1, 2, 3, 4], null, 2)}</pre>
+                <pre className="text-black">{JSON.stringify(user, null, 2)}</pre>
+                <pre className="text-black">{JSON.stringify(webApp, null, 2)}</pre>
+                
+            </div>
+        </main>
+    );
+}
