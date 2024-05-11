850228615aaa4dc37fc2ed00a4dc21f8e211dd3f
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-11 18:09:30 +0300

    feat: added api/bot as part of nextjs app

---
+++ src/app/api/bot/route.ts
@@ -0,0 +1,16 @@
+import { Bot, webhookCallback } from "grammy";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is unset");
+const bot = new Bot(token);
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+export const maxDuration = 50;
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
+
+export const POST = webhookCallback(bot, "std/http");
