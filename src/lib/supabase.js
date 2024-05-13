4b4cc8a55a27351515bc4f401909f05db617a49a
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-13 20:37:40 +0300

    fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -17,7 +17,30 @@ export const getUserById = async (id) => {
 };
 
 export const createUser = async (user) => {
-    const { data, error } = await supabase.from("tg_bot_users").upsert([
+    const { data: dbUser, error: userError } = await supabase
+        .from("tg_bot_users")
+        .select("*")
+        .eq("telegram_id", user.id);
+
+    if (dbUser) {
+        const { data, error } = await supabase
+            .from("tg_bot_users")
+            .update([
+                {
+                    created_at: new Date(),
+                    status: "active",
+                    username: user.username || null,
+                    first_name: user.first_name || null,
+                    last_name: user.last_name || null,
+                    is_premium: user.is_premium ? true : false,
+                    welcome_showed: true,
+                },
+            ])
+            .eq("telegram_id", id);
+
+        return data;
+    }
+    const { data, error } = await supabase.from("tg_bot_users").insert([
         {
             telegram_id: user.id || null,
             created_at: new Date(),
@@ -34,25 +57,24 @@ export const createUser = async (user) => {
 };
 
 export const addUserPhotoUrl = async (id, photo_url) => {
-    // const { data: user, error: userError } = await supabase
-    //     .from("tg_bot_users")
-    //     .select("*")
-    //     .eq("telegram_id", id);
-
-    // if (user) {
-    //     const { data, error } = await supabase
-    //         .from("tg_bot_users")
-    //         .update({ photo_url: photo_url })
-    //         .eq("telegram_id", id);
-
-    //     return data;
-    // }
-
-    const { data, error } = await supabase
+    const { data: user, error: userError } = await supabase
         .from("tg_bot_users")
-        .upsert({
-            telegram_id: id,
-            photo_url: photo_url })
+        .select("*")
+        .eq("telegram_id", id);
+
+    if (user) {
+        const { data, error } = await supabase
+            .from("tg_bot_users")
+            .update({ photo_url: photo_url })
+            .eq("telegram_id", id);
+
+        return data;
+    }
+
+    const { data, error } = await supabase.from("tg_bot_users").upsert({
+        telegram_id: id,
+        photo_url: photo_url,
+    });
 
     return data;
 };
