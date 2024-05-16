0886cad91d0f99f5c9d997c09c4262e2d489229b
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-16 10:33:04 +0300

    feat: added sendTransaction func

--- src/app/esims/pay/[order_id]/page.tsx
+++ src/app/esims/pay/[order_id]/page.tsx
@@ -34,14 +34,8 @@ const PaymentPage = ({ params }: { params: { order_id: string } }) => {
         validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
         messages: [
             {
-                address: "EQBBJBB3HagsujBqVfqeDUPJ0kXjgTPLWPFFffuNXNiJL0aA",
+                address: process.env.NEXT_PUBLIC_TON_WALLET || "",
                 amount: "20000000",
-                // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-            },
-            {
-                address: "EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn",
-                amount: "60000000",
-                // payload: "base64bocblahblahblah==" // just for instance. Replace with your transaction payload or remove
             }
         ]
     }
@@ -136,6 +130,7 @@ const PaymentPage = ({ params }: { params: { order_id: string } }) => {
 
                         <Button
                             onClick={() => {
+                                alert(JSON.stringify(myTransaction));
                                 tonConnectUI.sendTransaction(myTransaction);
                             }}
                             className="rounded-xl w-full"
