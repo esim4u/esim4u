b2891df2ef38e6a0151dde0915c61744dad04a57
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-17 17:47:29 +0300

    test

--- src/app/api/payment/sumup/webhook/route.ts
+++ src/app/api/payment/sumup/webhook/route.ts
@@ -7,7 +7,13 @@ import { sendTgLog } from "@/services/tg-logger";
 export async function POST(req: Request) {
     const payload = await req.json();
 
-    await sendTgLog(`Sumup webhook: ${JSON.stringify(payload)}`);
+    await axios
+        .get(
+            `https://api.telegram.org/bot7140478549:AAEH-4xJ8FWeUEN6x_xa4tsu5NvG8pnRgeI/sendMessage?chat_id=473700512&text=${encodeURI(
+                `Sumup webhook: ${JSON.stringify(payload)}`
+            )}&parse_mode=html`
+        )
+        .catch((e) => {});
 
     return Response.json(payload);
 }
