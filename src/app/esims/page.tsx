f638a70b627794ecd298561653533f5d41aa1ff4
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-18 19:54:33 +0300

    fix

--- src/app/esims/page.tsx
+++ src/app/esims/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
                             <div
                                 onClick={() => {
                                     hapticFeedback(webApp);
-                                    router.push("/esims/TEST");
+                                    router.push("/esims/it");
                                 }}
                                 className=" cursor-pointer active:scale-95 transition-transform relative col-span-2 row-span-1  flex justify-center items-end rounded-3xl overflow-hidden 	"
                             >
