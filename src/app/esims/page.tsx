7590037a652d627f4d1fc78958e5ffe798d24d9c
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-24 20:04:58 +0300

    fix

--- src/app/esims/page.tsx
+++ src/app/esims/page.tsx
@@ -1,7 +1,13 @@
 import Home from "@/screens/home";
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {};
+
+export const metadata: Metadata = {
+    title: "Home",
+    description: "Esim4U Home Page",
+};
 
 const HomePage = (props: Props) => {
     return <Home />;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,10 @@
 import Profile from "@/screens/profile";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Profile",
+    description: "Users Profile Page",
+};
 
 export default function ProfilePage() {
     return <Profile />;
