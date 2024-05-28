06de91d63d1fc327a5abd1741cb6d6c8b4ff017c
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-28 19:49:05 +0300

    fix: ts fix

--- src/components/shared/popular-countries.tsx
+++ src/components/shared/popular-countries.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { MdArrowForwardIos } from "react-icons/md";
-import AsyncImage from "../ui/async-image";
 
 type Props = {
     hasTitle?: boolean;
