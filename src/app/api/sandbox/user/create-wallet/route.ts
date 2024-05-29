28a827b7c6df686a44198872ddec5a0225710864
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-29 20:25:58 +0300

    fix

--- src/app/api/sandbox/user/create-wallet/route.ts
+++ src/app/api/sandbox/user/create-wallet/route.ts
@@ -1,10 +1,8 @@
-import { createWallet } from "@/services/supabase";
 
 export async function POST(req: Request) {
     const { telegram_id, wallet_address } = await req.json();
     try {
-        const data = await createWallet(telegram_id, wallet_address)
-        return Response.json(data);
+        return Response.json({ telegram_id, wallet_address });
     } catch (error) {
         return Response.json(error);
     }
