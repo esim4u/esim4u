e365de459c8e48d7c4012ab9ff97cce155295d02
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-05-31 20:23:06 +0300

    fix

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -97,7 +97,7 @@ export default function Profile() {
                 <RefLinkButton />
                 <Achievements fullWidth/>
                 <UserEsims />
-                <Referrals />
+                {/* <Referrals /> */}
             </div>
         </main>
     );
