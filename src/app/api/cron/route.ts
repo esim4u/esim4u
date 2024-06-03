32103c1e8d3e8d8c69bd78570c23ebf8f24de97e
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-03 14:41:45 +0300

    test

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,25 @@
+import { ORDER_STATUS } from "@/enums";
+import { supabase } from "@/services/supabase";
 import { sendTgLog } from "@/services/tg-logger";
 
 export async function GET() {
+    const orders = await supabase
+        .from("orders")
+        .select("*")
+        .in("status", [ORDER_STATUS.SUCCESS, ORDER_STATUS.PENDING]);
 
-    await sendTgLog("cron stated processing esims orders: " );
+    if (orders.error) {
+        await sendTgLog("An cron error occurred while fetching orders");
+
+        return Response.json(
+            {
+                message: "An error occurred while fetching orders",
+                description: orders.error.message,
+            },
+            { status: 500 }
+        );
+    }
+    await sendTgLog("cron stated processing esims orders: " + orders.data.map((o) => o.id).join(", "));
 
     const result = await fetch(
         "http://worldtimeapi.org/api/timezone/America/Chicago",
