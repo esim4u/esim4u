dbefbe3976ef3d7e9bd94c476cb9da0f6ccc2f53
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-03 14:35:56 +0300

    test

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,4 +1,9 @@
+import { sendTgLog } from "@/services/tg-logger";
+
 export async function GET() {
+
+    await sendTgLog("cron stated processing esims orders: " );
+
     const result = await fetch(
         "http://worldtimeapi.org/api/timezone/America/Chicago",
         {
