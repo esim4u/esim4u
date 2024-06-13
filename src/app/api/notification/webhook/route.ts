d7e065431dee28ff1be359c4e0fbaad6b859655b
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-13 19:35:18 +0300

    fix: set webhook as POST request instead of GET

--- src/app/api/notification/webhook/route.ts
+++ src/app/api/notification/webhook/route.ts
@@ -1,6 +1,6 @@
 import { sendTgLog } from "@/services/tg-logger";
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
 
     await sendTgLog("NOTIFICATION WEBHOOK PAYLOAD:");
 
