045dc64e7f0b921306bd79438b2e136399141b46
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-13 19:54:59 +0300

    fix

--- src/app/api/notification/webhook/route.ts
+++ src/app/api/notification/webhook/route.ts
@@ -1,6 +1,6 @@
 import { sendTgLog } from "@/services/tg-logger";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
 
     await sendTgLog("NOTIFICATION WEBHOOK PAYLOAD:");
 
