cf072f1ab9a1fd69b87e434f7a84c98f96cb6a8a
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-13 19:23:55 +0300

    fix: set notifications endpoint to GET request instead of POST

--- src/app/api/notification/webhook/route.ts
+++ src/app/api/notification/webhook/route.ts
@@ -1,6 +1,6 @@
 import { sendTgLog } from "@/services/tg-logger";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
     const payload = await req.json();
 
     await sendTgLog("NOTIFICATION WEBHOOK PAYLOAD: " + JSON.stringify(payload));
