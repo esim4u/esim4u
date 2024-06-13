151af6dc44bf629a35d8f18e9eb8c44c7a81d796
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-13 18:15:40 +0300

    fix:  page with tonconnect is fully client rendered

--- src/app/(root)/donation/page.tsx
+++ src/app/(root)/donation/page.tsx
@@ -1,11 +1,13 @@
-import Donation from "@/screens/payment/donation";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
 
 export const metadata: Metadata = {
     title: "Donation",
     description: "Esim4U Donation Page",
 };
-
+const Donation = dynamic(() => import("@/screens/payment/donation"), {
+    ssr: false,
+});
 export default function DonationPage() {
     return <Donation />;
 }
