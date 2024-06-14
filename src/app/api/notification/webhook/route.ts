fc1fb5d196260ce271317c0e22ef7179bffc1444
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-14 19:13:33 +0300

    test: notification webhook test

--- src/app/api/notification/webhook/route.ts
+++ src/app/api/notification/webhook/route.ts
@@ -1,8 +1,14 @@
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
 import { sendTgLog } from "@/services/tg-logger";
 
-export async function GET(req: Request) {
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const iccid = searchParams.get("iccid");
 
-    await sendTgLog("NOTIFICATION WEBHOOK PAYLOAD:");
+    await sendTgLog(
+        "NOTIFICATION WEBHOOK PAYLOAD:" + JSON.stringify({ iccid }),
+    );
 
-    return Response.json( { status: 200 });
+    return Response.json({ status: 200 });
 }
