5318a554b4758b262703af3898e2109b877e5297
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-19 11:52:36 +0300

    fix: fixed telegram closing modal when scrolling down

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,15 +1,17 @@
 import "../globals.css";
-import { Inter as FontSans } from "next/font/google";
 
-import { cn } from "@/lib/utils";
 import { Metadata, Viewport } from "next";
-import { TelegramProvider } from "@/providers/telegram-provider";
+import { Inter as FontSans } from "next/font/google";
 import Script from "next/script";
 import ReactQueryProvider from "@/providers/query-provider";
+import { TelegramProvider } from "@/providers/telegram-provider";
 import TonConnectProvider from "@/providers/tonconnect-provider";
-import { Toaster } from "@/components/ui/toaster";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
+
+import { cn } from "@/lib/utils";
+
+import { Toaster } from "@/components/ui/toaster";
 
 const fontSans = FontSans({
     subsets: ["latin"],
@@ -41,8 +43,8 @@ export default function RootLayout({
                     <html lang="en" suppressHydrationWarning>
                         <body
                             className={cn(
-                                "min-h-screen w-screen font-sans antialiased overflow-x-hidden bg-background no-select",
-                                fontSans.variable
+                                "no-select  h-screen w-screen overflow-hidden bg-background font-sans antialiased",
+                                fontSans.variable,
                             )}
                         >
                             <Script
@@ -54,7 +56,9 @@ export default function RootLayout({
                                 strategy="beforeInteractive"
                             /> */}
                             <Script src="https://gateway.sumup.com/gateway/ecom/card/v2/sdk.js" />
-                            {children}
+                            <div className=" absolute bottom-0 left-0 right-0 top-0 overflow-y-auto overflow-x-hidden ">
+                                <div style={{height: "calc(100% + 1px)"}}>{children}</div>
+                            </div>
                             <Toaster />
                             <Analytics />
                         </body>
