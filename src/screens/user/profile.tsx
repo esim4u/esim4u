afb1a29eb833403c7178d30a3100425bcb047c04
Vasyl Vezhdel <vezhdelit@gmail.com>
2024-06-28 12:38:53 +0300

    style: adjusted profile page visuals

--- src/screens/user/profile.tsx
+++ src/screens/user/profile.tsx
@@ -8,6 +8,7 @@ import { sendGAEvent } from "@next/third-parties/google";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { track } from "@vercel/analytics/react";
 import { IoIosSettings } from "react-icons/io";
+import { IoQrCode } from "react-icons/io5";
 
 import { l } from "@/lib/locale";
 import { cn, hapticFeedback, shareRef } from "@/lib/utils";
@@ -82,23 +83,16 @@ export default function Profile() {
     }, [webApp]);
 
     return (
-        <main className="no-scrollbar flex h-dvh flex-col items-center overflow-x-hidden p-5">
+        <main className="no-scrollbar flex h-dvh flex-col items-center overflow-x-hidden px-5 py-2">
             <div className="relative flex w-full flex-col items-center gap-4">
                 <IoIosSettings
                     onClick={() => {
                         hapticFeedback();
                         router.push("/settings");
                     }}
-                    className="absolute left-0 h-12 w-12 cursor-pointer text-neutral-400"
+                    className="absolute left-0 top-8 h-12 w-12 cursor-pointer text-neutral-400"
                 />
-                {/* <IoQrCode
-                    onClick={() => {
-                        hapticFeedback();
-                        router.push("/profile/qr");
-                    }}
-                    className="absolute right-0 h-12 w-12 cursor-pointer py-1 text-neutral-400"
-                /> */}
-                <WalletBanner className="absolute -right-5" />
+                <WalletBanner className="absolute -right-5 top-8" />
 
                 <div className="flex flex-col items-center gap-2">
                     <Avatar className="h-32 w-32  ring-[3px] ring-neutral-400/30 ring-offset-2">
@@ -119,7 +113,19 @@ export default function Profile() {
                     </h2>
                     <Badge size={"md"}>{dbUserData?.badge}</Badge>
                 </div>
-                <RefLinkButton />
+                <div className="flex flex-row items-center gap-2">
+                    <RefLinkButton />
+
+                    <button className=" active:scale-90 flex h-10 w-10 items-center justify-center rounded-full bg-white">
+                        <IoQrCode
+                            onClick={() => {
+                                hapticFeedback();
+                                router.push("/profile/qr");
+                            }}
+                            className=" h-7 w-7 cursor-pointer py-1 text-neutral-400"
+                        />
+                    </button>
+                </div>
                 <Achievements fullWidth />
                 <Suspense fallback={<div></div>}>
                     <UserEsims />
@@ -153,13 +159,15 @@ const WalletBanner = ({ className }: { className?: string }) => {
                 router.push("/profile/wallet");
             }}
             className={cn(
-                "active:scale-95  origin-right flex flex-row shadow-lg shadow-black/5 items-center justify-center gap-2 rounded-l-2xl bg-white px-3 py-1.5",
+                "flex  origin-right flex-row items-center justify-center gap-2 rounded-l-2xl bg-white px-3 py-1.5 shadow-lg shadow-black/5 active:scale-95",
                 className,
             )}
         >
             <WalletIcon className={" h-9 w-9"} />
             <div className="flex items-center justify-center text-blue-500">
-                <span className="text-[22px]  font-bold">{walletData?.amount}</span>
+                <span className="text-[22px]  font-bold">
+                    {walletData?.amount}
+                </span>
                 <TonIcon className="h-4 w-4" />
             </div>
         </button>
