04c0881cb35e26ea9f10ceddca43b7b86d94b569
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-11-22 12:37:19 +0200

    fix

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,33 +1,21 @@
+export const maxDuration = 50;
 export const dynamic = "force-dynamic";
-export const revalidate = 3600;
+export const dynamicParams = true;
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export async function GET() {
     let response = await fetch(
         process.env.NEXT_PUBLIC_WEB_APP_URL + "/api/esims/sync",
         {
             cache: "no-store",
-        }
+        },
     );
 
-    if (!response.ok) {
-        return Response.json(
-            {
-                message: "An error occurred while fetching orders",
-                description: response.statusText,
-            },
-            { status: 500 }
-        );
-    }
-    const result = await response.json();
-
-    console.log(
-        "cron finished processing esims orders: " +
-            result.data.map((o: any) => o.id).join(", ")
-    );
     return Response.json(
         {
-            result: result.data,
+            message: "Cron job executed successfully",
         },
-        { status: 200 }
+        { status: 200 },
     );
 }
