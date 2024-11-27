bf493ec44d4c0ae92e92ecf5be4b15d6ef8aee3a
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-11-27 15:12:16 +0200

    fix

--- src/providers/tonconnect-provider.tsx
+++ src/providers/tonconnect-provider.tsx
@@ -1,20 +1,20 @@
 "use client";
 
 import React, { useMemo } from "react";
-import { TonConnectUIProvider } from "@tonconnect/ui-react";
+// import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const TonConnectProvider = ({ children }: { children: React.ReactNode }) => {
-	const manifestUrl = useMemo(() => {
-		return new URL(
-			"tonconnect-manifest.json",
-			window.location.href
-		).toString();
-	}, []);
+	// const manifestUrl = useMemo(() => {
+	// 	return new URL(
+	// 		"tonconnect-manifest.json",
+	// 		window.location.href
+	// 	).toString();
+	// }, []);
 
 	return (
-		<TonConnectUIProvider manifestUrl={manifestUrl}>
+		// <TonConnectUIProvider manifestUrl={manifestUrl}>
 			{children}
-		</TonConnectUIProvider>
+		// </TonConnectUIProvider>
 	);
 };
 
