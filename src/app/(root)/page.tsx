c04db092296e360b2a76c3cb71c5ceb59a288e89
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-11-27 12:03:13 +0200

    test: deploy test

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,43 +1,8 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { getUserById, updateUser } from "@/services/supabase";
-
 import Loader from "@/components/ui/loader";
-import { initData, useLaunchParams, useSignal } from "@telegram-apps/sdk-react";
 
 export default function IndexPage() {
-    const router = useRouter();
-    const initDataState = useSignal(initData.state);
-	const lp = useLaunchParams();
-
-
-
-    useEffect(() => {
-        const fetchUser = async (tgUser: any) => {
-            const dbUser = await getUserById(tgUser.id);
-    
-            if (dbUser?.id) {
-                await updateUser(tgUser, dbUser);
-                // return router.push("/esims");
-            }
-    
-            // return router.push("/onboarding");
-        };
-
-        if (initDataState && lp) {
-            const tgUser = {
-                ...initDataState.user,
-                start_param: initData.startParam,
-                platform: lp?.platform,
-            }
-            fetchUser(tgUser.id);
-        }
-    }, [initDataState, lp, router]);
-
-
-
     return (
         <main className="container flex h-screen items-center justify-center bg-white py-5">
             <Loader />
