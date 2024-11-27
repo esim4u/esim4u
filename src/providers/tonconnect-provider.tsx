e444ae1feab6309ef79f7a8ca33505d61ee20e0e
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-11-27 15:04:25 +0200

    fix

--- src/providers/tonconnect-provider.tsx
+++ src/providers/tonconnect-provider.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const WEB_APP_URL = process.env.NEXT_PUBLIC_WEB_APP_URL;
-
 const TonConnectProvider = ({ children }: { children: React.ReactNode }) => {
+	const manifestUrl = useMemo(() => {
+		return new URL(
+			"tonconnect-manifest.json",
+			window.location.href
+		).toString();
+	}, []);
+
 	return (
-		<TonConnectUIProvider
-			manifestUrl={WEB_APP_URL + "/tonconnect-manifest.json"}
-			actionsConfiguration={{
-				twaReturnUrl: "https://t.me/esim4u_test_bot",
-			}}
-		>
+		<TonConnectUIProvider manifestUrl={manifestUrl}>
 			{children}
 		</TonConnectUIProvider>
 	);
