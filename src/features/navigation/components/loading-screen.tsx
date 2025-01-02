bf226369c990195fe9dd027bd71a537458e91e7c
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-01-02 16:31:03 +0200

    test

--- src/features/navigation/components/loading-screen.tsx
+++ src/features/navigation/components/loading-screen.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import amongUsAnim from "@/assets/anim/among-us.json";
-import Lottie from "lottie-react";
 
 const LoadingScreen = () => {
 	return (
 		<main className="container flex items-center justify-center py-5 bg-background h-screen">
 			<div className={"w-56 h-56"}>
-				<Lottie className="-mt-14" animationData={amongUsAnim} />
+				Loading
 			</div>
 		</main>
 	);
