60924da04f366bca11b9a14d5849128f4f4489fc
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-01-02 16:47:47 +0200

    test

--- src/features/navigation/components/loading-screen.tsx
+++ src/features/navigation/components/loading-screen.tsx
@@ -1,14 +1,17 @@
-"use client"
+"use client";
 
 import React from "react";
 import amongUsAnim from "@/assets/anim/among-us.json";
 import Lottie from "lottie-react";
+import { Suspense } from "react";
 
 const LoadingScreen = () => {
 	return (
 		<main className="container flex items-center justify-center py-5 bg-background h-screen">
 			<div className={"w-56 h-56"}>
-				<Lottie className="-mt-14" animationData={amongUsAnim} />
+				<Suspense fallback={<div></div>}>
+					<Lottie className="-mt-14" animationData={amongUsAnim} />
+				</Suspense>
 			</div>
 		</main>
 	);
