7cafc373e2535f209c17cc6b059d14e3b0e94b77
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-01-08 14:14:02 +0200

    style: adjusted bottom sticky button styling

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -31,7 +31,7 @@ const buttonVariants = cva(
 				default: "h-10 px-4 py-2",
 				sm: "h-9 rounded-md px-3",
 				lg: "h-11 rounded-md px-8",
-				xl: "h-12 rounded-xl px-12",
+				xl: "h-12 rounded-xl px-12 text-base",
 				icon: "h-10 w-10",
 				bean: "h-12 px-8 rounded-2xl min-w-24 text-xl capitalize font-medium",
 			},
--- src/features/navigation/components/bottom-sticky-button.tsx
+++ src/features/navigation/components/bottom-sticky-button.tsx
@@ -7,10 +7,10 @@ type Props = {} & React.ComponentProps<typeof Button> & ButtonProps;
 const BottomStickyButton = (props: Props) => {
 	return (
 		<>
-			<div className="h-12 pt-1.5 safe-area-bottom w-full"></div>
+			<div className="h-12 pt-2 safe-area-bottom w-full"></div>
 			<div
 				className={cn(
-					"container fixed bottom-0 p-0 pt-1.5 w-full z-20 bg-background border-t-[1px] border-neutral-300 safe-area-bottom"
+					"container fixed bottom-0 p-0 pt-2 w-full z-20 bg-background border-t-[1px] border-neutral-300 safe-area-bottom"
 				)}
 			>
 				<div className="px-4">
@@ -18,7 +18,7 @@ const BottomStickyButton = (props: Props) => {
 						{...props}
 						variant={"telegram"}
 						size={"xl"}
-						className={cn("rounded-2xl w-full", props.className)}
+						className={cn("rounded-xl  w-full", props.className)}
 					>
 						{props.children}
 					</Button>
