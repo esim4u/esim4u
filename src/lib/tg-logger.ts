fdb9b5ddaff9798e324fd5ecf9fb6e15fa4e6fa9
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-01-19 01:23:03 +0200

    fix: using fetch instead of axios for tg logs

--- src/lib/tg-logger.ts
+++ src/lib/tg-logger.ts
@@ -11,15 +11,6 @@ const admins = [473700512, 258793];
 
 export const sendTgLog = async (message: string) => {
 	members.forEach(async (chat_id) => {
-		// await axios
-		// 	.get(
-		// 		`https://api.telegram.org/bot${TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
-		// 			message
-		// 		)}&parse_mode=html`
-		// 	)
-		// 	.catch((e) => {});
-
-		// use fetch instead of axios
 		await fetch(
 			`https://api.telegram.org/bot${TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
 				message
@@ -30,12 +21,10 @@ export const sendTgLog = async (message: string) => {
 
 export const sendAdminTgLog = async (message: string) => {
 	admins.forEach(async (chat_id) => {
-		await axios
-			.get(
-				`https://api.telegram.org/bot${ADMIN_TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
-					message
-				)}&parse_mode=html`
-			)
-			.catch((e) => {});
+		await fetch(
+			`https://api.telegram.org/bot${ADMIN_TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
+				message
+			)}&parse_mode=html`
+		).catch((e) => {});
 	});
 };
