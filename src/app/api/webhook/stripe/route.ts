a4a4bff8518fe55c1d53215de2757a093027940f
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-01-19 00:54:46 +0200

    fix: webhook fix

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,7 +1,13 @@
-import { sendAdminTgLog, sendTgLog } from "@/lib/tg-logger";
+import { sendTgLog } from "@/lib/tg-logger";
 
 export async function POST(req: Request) {
 	const body = await req.json();
 	console.log(body);
 	await sendTgLog(JSON.stringify(body, null, 2));
+	return Response.json(
+		{
+			message: "Successfully logged the webhook event",
+		},
+		{ status: 200 }
+	);
 }
