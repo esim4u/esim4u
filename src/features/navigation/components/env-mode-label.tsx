a5ea27e8f1a18f61aad3636ce07b6070c8495133
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-02-12 17:22:30 +0200

    style: env label style change

--- src/features/navigation/components/env-mode-label.tsx
+++ src/features/navigation/components/env-mode-label.tsx
@@ -5,8 +5,9 @@ const EnvModeLabel = () => {
 	if (clientEnvs.NEXT_PUBLIC_ENV_MODE == "production") return null;
 	return (
 		<div className="fixed left-1/2 -translate-x-1/2 container overflow-hidden z-50 h-16  p-0">
-			<div className="absolute bg-redish px-2.5 py-0.5 rounded-br-xl">
-				<h3 className=" font-mono font-bold text-white">DEV BUILD</h3>
+			<div className=" flex justify-between w-fit rounded-b-xl bg-redish px-5 py-1.5">
+				<h3 className=" font-mono font-bold text-white">DEV</h3>
+				{/* <h3 className=" font-mono font-bold text-white">BUILD</h3> */}
 			</div>
 		</div>
 	);
