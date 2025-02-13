31eb5d11166ba199b71fb82932ef3acd36e0da0b
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-02-13 15:05:28 +0200

    fix: not sended telegram messages fix

--- src/lib/tg-logger.ts
+++ src/lib/tg-logger.ts
@@ -9,21 +9,25 @@ const ADMIN_TG_LOGGER_BOT_TOKEN = serverEnvs.ADMIN_BOT_TOKEN;
 const admins = [473700512, 258793];
 
 export const sendTgLog = async (message: string) => {
-	members.forEach(async (chat_id) => {
-		await fetch(
-			`https://api.telegram.org/bot${TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
-				message
-			)}&parse_mode=html`
-		).catch((e) => {});
-	});
+	await Promise.all(
+		members.map((chat_id) =>
+			fetch(
+				`https://api.telegram.org/bot${TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURIComponent(
+					message
+				)}&parse_mode=html`
+			).catch((e) => console.error("Error sending Telegram message:", e))
+		)
+	);
 };
 
 export const sendAdminTgLog = async (message: string) => {
-	admins.forEach(async (chat_id) => {
-		await fetch(
-			`https://api.telegram.org/bot${ADMIN_TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURI(
-				message
-			)}&parse_mode=html`
-		).catch((e) => {});
-	});
+	await Promise.all(
+		admins.map((chat_id) =>
+			fetch(
+				`https://api.telegram.org/bot${ADMIN_TG_LOGGER_BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${encodeURIComponent(
+					message
+				)}&parse_mode=html`
+			).catch((e) => console.error("Error sending Telegram message:", e))
+		)
+	);
 };
