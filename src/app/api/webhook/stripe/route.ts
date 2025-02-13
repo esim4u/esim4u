4799a13fb8da41317d978ecf7630f5b70439c904
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-02-13 15:13:57 +0200

    test: using next/server to sendTgLog without blocking an request

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -1,9 +1,15 @@
 import { sendTgLog } from "@/lib/tg-logger";
+import { after } from "next/server";
 
 export async function POST(req: Request) {
 	const body = await req.json();
 	console.log(body);
-	await sendTgLog(JSON.stringify(body, null, 2));
+	// Schedule a side-effect after the response is sent
+	after(async () => {
+		// For example, log or increment analytics in the background
+		await sendTgLog(JSON.stringify(body, null, 2));
+	});
+	
 	return Response.json(
 		{
 			message: "Successfully logged the webhook event",
