2fb79cbdcb19319ecab70bb091b0e79866d409b6
Vezhdel Vasyl <vezhdelit@gmail.com>
2025-02-19 16:10:04 +0200

    fix: remove SUMUP env vars

--- src/env/server.ts
+++ src/env/server.ts
@@ -7,11 +7,6 @@ export const serverEnvs = createEnv({
 		AIRALO_CLIENT_ID: z.string(),
 		AIRALO_CLIENT_SECRET: z.string(),
 
-		SUMUP_SECRET_KEY: z.string(),
-		SUMUP_APP_ID: z.string(),
-		SUMUP_PROD_MERCHANT: z.string(),
-		SUMUP_API_URL: z.string(),
-
 		STRIPE_SECRET_KEY: z.string(),
 
 		TG_LOGGER_BOT_TOKEN: z.string(),
