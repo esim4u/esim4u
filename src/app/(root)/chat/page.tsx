3c42d20778db4a640109893387d91b1276fd4a57
vezhdelit <vezhdelit@gmail.com>
2025-03-18 14:30:28 +0200

    test

--- src/app/(root)/chat/page.tsx
+++ src/app/(root)/chat/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChatPage = () => {
 	return (
-		<div className="w-screen h-dvh">
+		<div className="w-screen h-[70vh]">
 			<iframe
 				className="w-full h-full"
 				src="https://jivo.chat/TROIdShs2z"
